cc56eb8e949763e9872bba584ad8f7bf941b3b44
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2024-07-30 11:09:53 +0600

    create order api

--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -1,9 +1,46 @@
+import { ProductModel } from "../product/product.model";
 import { IOrder } from "./order.interface";
 import { OrderModel } from "./order.model";
 
 const createOrder = async (order: IOrder) => {
-  const newOrder = await OrderModel.create(order);
-  return newOrder;
+  // Start a session for transaction
+  const session = await OrderModel.startSession();
+  session.startTransaction();
+
+  try {
+    // Create the order
+    const newOrder = await OrderModel.create([order], { session });
+
+    // Reduce the stock for each product
+    for (const product of order.products) {
+      const updatedProduct = await ProductModel.findByIdAndUpdate(
+        product._id, // Assuming product._id refers to the product in the ProductModel
+        { $inc: { stock: -product.quantity } },
+        { new: true, session }
+      );
+
+      if (!updatedProduct) {
+        throw new Error(`Product with id ${product._id} not found`);
+      }
+
+      if (updatedProduct.stock < 0) {
+        throw new Error(
+          `Insufficient stock for product ${updatedProduct.name}`
+        );
+      }
+    }
+
+    // Commit the transaction
+    await session.commitTransaction();
+    session.endSession();
+
+    return newOrder;
+  } catch (error) {
+    // Rollback the transaction in case of error
+    await session.abortTransaction();
+    session.endSession();
+    throw error;
+  }
 };
 
 export const OrderServices = {
